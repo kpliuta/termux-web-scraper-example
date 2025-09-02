59d52c22cb26794a1a63198d025924036c88a572
kpliuta <9212935+kpliuta@users.noreply.github.com>
2025-09-03 00:12:33 +0200

    Add scaffolding for simple scenario. Implement simple/run.sh

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # termux-web-scraper-example
+
+Prerequisites are the same as for termux-web-scraper
+
+Execution:
+- launch termux
+- checkout this repo
+- execute run.sh in an any of the subfolders (with represents a particular scenario)
---
+++ simple/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Check if git is installed
+if ! command -v git >/dev/null 2>&1; then
+    echo "Error: git is not installed. Please install it and try again."
+    exit 1
+fi
+
+# Get the absolute path to the scenario directory
+WORK_DIR=$(realpath "$(dirname "$0")")
+
+# Clone the repository
+git clone https://github.com/kpliuta/termux-web-scraper.git "$WORK_DIR/termux-web-scraper"
+
+# Execute the scraper script
+"$WORK_DIR/termux-web-scraper/scripts/run.sh" \
+    --scenarios-dir "$WORK_DIR/selenium" \
+    --script simple.py \
+    --upgrade \
+    --output-dir /sdcard/termux-web-scraper:/mnt/scraper/out
