dadf349f893c8a15539556ccd08a0daa962ea267
kpliuta <9212935+kpliuta@users.noreply.github.com>
2025-09-11 18:41:52 +0200

    Specify a version of termux-web-scraper

--- simple/run.sh
+++ simple/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+TERMUX_WEB_SCRAPER_VERSION="0.1.0"
+
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
@@ -12,16 +14,12 @@ fi
 # Get the absolute path to the scenario directory.
 WORK_DIR=$(realpath "$(dirname "$0")")
 
-# Clone or update the termux-web-scraper repository.
-if [ -d "$WORK_DIR/termux-web-scraper" ]; then
-  echo "Updating termux-web-scraper..."
-  git -C "$WORK_DIR/termux-web-scraper" pull --rebase
-else
+# Clone the termux-web-scraper repository.
+if [ ! -d "$WORK_DIR/termux-web-scraper" ]; then
   echo "Cloning termux-web-scraper..."
-  # TODO: specify a version of termux-web-scraper
   git clone https://github.com/kpliuta/termux-web-scraper.git "$WORK_DIR/termux-web-scraper"
+  git -C "$WORK_DIR/termux-web-scraper" checkout "$TERMUX_WEB_SCRAPER_VERSION"
 fi
-
 
 # Execute the scraper script.
 "$WORK_DIR/termux-web-scraper/scripts/run.sh" \
